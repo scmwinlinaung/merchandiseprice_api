783cb7e72e6204986bdc2ebf1f1b35173d3b5a62
winlinaung <winlinaung@192.168.1.5>
2025-12-31 09:08:19 +0630

    update a bit

--- config/config.js
+++ config/config.js
@@ -8,12 +8,6 @@ module.exports = {
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: false,
-        rejectUnauthorized: false,
-      },
-    },
     logging: false,
   },
   test: {
@@ -23,24 +17,19 @@ module.exports = {
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: false,
-        rejectUnauthorized: false,
-      },
-    },
     logging: false,
   },
   production: {
     username: process.env.DATABASE_USERNAME || 'your_production_username',  // Use environment variable or fallback
     password: process.env.DATABASE_PASSWORD || 'your_production_password',
     database: process.env.DATABASE_NAME,
+    schema: 'myan_market',
     host: process.env.DATABASE_HOST || 'your_production_host',
     port: process.env.DATABASE_PORT || 5432,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true,
+        require: false,
         rejectUnauthorized: false,  // Set to true if using a CA certificate
         // Uncomment the following if you have a CA certificate
         // ca: fs.readFileSync('path/to/rds-ca.pem'),  // Path to the CA certificate
